75aa0b980f4afe8616da5a96681bb8bd4cfcbb4f
Harsh! <126320221+harshh0107@users.noreply.github.com>
2024-12-19 11:01:07 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+1) Install dependencies
+
+```sh
+pip install -r requirements.txt
+```
+
+2) Change sender's email address and password in `.env`
+> Follow the instructions in `CreatingAppPasswordForGoogleAccount.pdf` to create a new app password, make sure to **remove any spaces** in the copied password.
+
+3) Change recipient's email address in email list
+> line number: 54 in main.py
+
+4) Run the code 
+
+```sh
+python main.py
+```
+
+> Note: Wait for "**listening...**" to appear in the terminal before you speak.
---
+++ main_att.py
@@ -0,0 +1,109 @@
+import smtplib
+import speech_recognition as sr
+import pyttsx3
+from email.message import EmailMessage
+import tkinter as tk
+from tkinter import filedialog
+from os import path
+
+with open(".env") as file:
+    lines = file.readlines()
+    SENDER_EMAIL = lines[0].split("=")[1].strip()
+    SENDER_PASSWORD = lines[1].split("=")[1].strip()
+
+print(f"Sender's Email: {SENDER_EMAIL}")
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+
+def talk(text):
+    print(text)
+    engine.say(text)
+    engine.runAndWait()
+
+def get_info() -> str:
+    with sr.Microphone() as source:
+        print("listening...")
+        voice = listener.listen(source, timeout=8)
+        print("processing...")
+        try:
+            info: str = listener.recognize_google(voice)
+            print("You said:", info)
+        except sr.UnknownValueError:
+            print("Could not understand audio.")
+            info = ""
+        except sr.RequestError as e:
+            print(
+                f"Could not request results from Google Speech Recognition service; {e}"
+            )
+            info = ""
+    return info.lower()
+
+def send_email(receiver, subject, message, attachments=None):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(SENDER_EMAIL, SENDER_PASSWORD)
+    email = EmailMessage()
+    email["From"] = SENDER_EMAIL
+    email["To"] = receiver
+    email["subject"] = subject
+    email.set_content(message)
+
+    if attachments:
+        for file in attachments:
+            with open(file, "rb") as f:
+                file_data = f.read()
+                file_name = path.basename(f.name)
+            email.add_attachment(file_data, maintype="application", subtype="octet-stream", filename=file_name)
+
+    server.send_message(email)
+    server.quit()
+
+email_list = {
+    # Change this to actual name and email address
+    # You can have multiple names and emails here.
+    "puja": "turlakonda.pooja@gmail.com",
+    "harshini": "harshinipolineni@gmail.com"
+}
+
+def get_email_info():
+    talk("Who's the email for?")
+    name: str = get_info()
+
+    receiver: str = email_list.get(name)
+    if not receiver:
+        talk("I couldn't find the email address for that name.")
+        return
+
+    talk("What is the subject of your email?")
+    subject: str = get_info()
+
+    talk("What is the body of your email?")
+    message: str = get_info()
+
+    talk("Do you want to add any attachments?")
+    add_attachments = get_info()
+
+    attachments = []
+    if "narnia" in add_attachments:
+        root = tk.Tk()
+        root.withdraw()
+        root.attributes('-topmost', True)
+        file_paths = filedialog.askopenfilenames()
+        attachments = list(file_paths)
+
+    talk("Sending...")
+    send_email(receiver, subject, message, attachments)
+
+    talk("Your email is sent.")
+
+    talk("Do you wish to send another email?")
+    send_more = get_info()
+    if "narnia" in send_more:
+        get_email_info()
+    elif "no" in send_more:
+        talk("Okay, Bye.")
+    else:
+        talk("Please confirm either by saying 'narnia' or 'no'")
+
+get_email_info()
